db93c9497a5041238a89af6ed47eabb5f58fc7d0
balanviorel <balanviorel2@gmail.com>
2025-06-04 13:39:51 +0300

    modifiing

--- src/Hi.java
+++ src/Hi.java
@@ -4,5 +4,15 @@ public class Hi {
         System.out.println("Hello");
         System.out.println("Hi");
         System.out.println("Hello, Hello!");
+
+        int a, b, i;
+        i=0;
+        for (a=0; a<=10; a++) {
+            i++;
+            //System.out.println(i + " " + a);
+            if (a % 2 == 0) {
+                System.out.println(a);
+            }
+        }
     }
 }
