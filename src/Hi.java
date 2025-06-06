ebe1c391c72412322fd29bd088452fadad1fb8c6
balanviorel <balanviorel2@gmail.com>
2025-06-06 12:46:36 +0300

    Hi

--- src/Hi.java
+++ src/Hi.java
@@ -5,14 +5,29 @@ public class Hi {
         System.out.println("Hi");
         System.out.println("Hello, Hello!");
 
-        int a, b, i;
-        i=0;
-        for (a=0; a<=10; a++) {
-            i++;
+        int a, b=0, i;
+        //i=0;
+        //for (a=-5; a<=20; a++) {
+            //i++;
             //System.out.println(i + " " + a);
-            if (a % 2 == 0) {
-                System.out.println(a);
-            }
+            //if (a % 2 == 0) {
+              //  System.out.println(a);
+           // }
+             //if (a % 2 != 0 && a<=15) {
+              //  System.out.println(a);
+            //}
+            // if (a<0) {
+            //     System.out.println(a);
+          //   }
+        for (a=0; a<=10; a++) {
+            b = b + a;
+
+
         }
+        System.out.println(b);
+        for (i=20; i<=30; i++) {
+            System.out.println(i);
+        }
+
     }
 }
